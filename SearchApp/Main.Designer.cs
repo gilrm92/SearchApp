bf0cefac9f66af01cce622ed38b6beed92353cde
gilrm92 <gilbertorm.ramos@gmail.com>
2017-09-03 18:24:17 +0200

    First commit

---
+++ SearchApp/Main.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SearchApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLocalization = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxLstItemsFound = new System.Windows.Forms.CheckedListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDoSearch = new System.Windows.Forms.Button();
+            this.btnSaveFiles = new System.Windows.Forms.Button();
+            this.btnCheckAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Localization";
+            // 
+            // txtLocalization
+            // 
+            this.txtLocalization.Location = new System.Drawing.Point(81, 6);
+            this.txtLocalization.Name = "txtLocalization";
+            this.txtLocalization.Size = new System.Drawing.Size(634, 20);
+            this.txtLocalization.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Filter";
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(81, 56);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(131, 20);
+            this.txtFilter.TabIndex = 3;
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(81, 32);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(634, 20);
+            this.txtDestination.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Destination";
+            // 
+            // cbxLstItemsFound
+            // 
+            this.cbxLstItemsFound.FormattingEnabled = true;
+            this.cbxLstItemsFound.Location = new System.Drawing.Point(12, 145);
+            this.cbxLstItemsFound.Name = "cbxLstItemsFound";
+            this.cbxLstItemsFound.Size = new System.Drawing.Size(703, 274);
+            this.cbxLstItemsFound.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Items found";
+            // 
+            // btnDoSearch
+            // 
+            this.btnDoSearch.Location = new System.Drawing.Point(12, 94);
+            this.btnDoSearch.Name = "btnDoSearch";
+            this.btnDoSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnDoSearch.TabIndex = 8;
+            this.btnDoSearch.Text = "Do search";
+            this.btnDoSearch.UseVisualStyleBackColor = true;
+            this.btnDoSearch.Click += new System.EventHandler(this.btnDoSearch_Click);
+            // 
+            // btnSaveFiles
+            // 
+            this.btnSaveFiles.Location = new System.Drawing.Point(12, 427);
+            this.btnSaveFiles.Name = "btnSaveFiles";
+            this.btnSaveFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveFiles.TabIndex = 9;
+            this.btnSaveFiles.Text = "Save selected files";
+            this.btnSaveFiles.UseVisualStyleBackColor = true;
+            this.btnSaveFiles.Click += new System.EventHandler(this.btnSaveFiles_Click);
+            // 
+            // btnCheckAll
+            // 
+            this.btnCheckAll.Location = new System.Drawing.Point(640, 116);
+            this.btnCheckAll.Name = "btnCheckAll";
+            this.btnCheckAll.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckAll.TabIndex = 10;
+            this.btnCheckAll.Text = "Check all";
+            this.btnCheckAll.UseVisualStyleBackColor = true;
+            this.btnCheckAll.Click += new System.EventHandler(this.btnCheckAll_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 478);
+            this.Controls.Add(this.btnCheckAll);
+            this.Controls.Add(this.btnSaveFiles);
+            this.Controls.Add(this.btnDoSearch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbxLstItemsFound);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLocalization);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "SearchApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLocalization;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox cbxLstItemsFound;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDoSearch;
+        private System.Windows.Forms.Button btnSaveFiles;
+        private System.Windows.Forms.Button btnCheckAll;
+    }
+}
+
---
+++ SearchApp/Main.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchApp
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoSearch_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtLocalization.Text) && Directory.Exists(txtLocalization.Text))
+            {
+                string localization = txtLocalization.Text;
+                string filter = !string.IsNullOrWhiteSpace(txtFilter.Text) ? txtFilter.Text : "*";
+
+                IList<string> files = Directory.GetFiles(localization, filter, SearchOption.AllDirectories)
+                                                    .Where(file => !new FileInfo(file).Attributes.HasFlag(FileAttributes.System | FileAttributes.Hidden))
+                                                    .ToList();
+
+                cbxLstItemsFound.DataSource = files;
+            }
+            else
+            {
+                MessageBox.Show("Type some location.", "Woops", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnCheckAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cbxLstItemsFound.Items.Count; i++)
+            {
+                cbxLstItemsFound.SetItemChecked(i, true);
+            }
+        }
+
+        private void btnSaveFiles_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtDestination.Text))
+            {
+                string destinationPath = txtDestination.Text;
+                IList<string> checkedFiles = new List<string>();
+
+                for (int i = 0; i < cbxLstItemsFound.Items.Count; i++)
+                {
+                    if (cbxLstItemsFound.GetItemChecked(i))
+                    {
+                        checkedFiles.Add((string)cbxLstItemsFound.Items[i]);
+                    }
+                }
+
+                foreach (string sourceFile in checkedFiles)
+                {
+                    FileInfo file = new FileInfo(sourceFile);
+                    string directoryDestination = file.DirectoryName.Replace("\\", "-").Substring(3);
+                    directoryDestination = Path.Combine(destinationPath, directoryDestination);
+
+                    if (!Directory.Exists(directoryDestination))
+                    {
+                        Directory.CreateDirectory(directoryDestination);
+                    }
+
+                    string destinationFile = Path.Combine(directoryDestination, file.Name);
+                    File.Copy(sourceFile, destinationFile, false);
+                }
+
+                MessageBox.Show("Copy finished", "Hey Ya", MessageBoxButtons.OK);
+
+                cbxLstItemsFound.DataSource = null;
+            }
+            else
+            {
+                MessageBox.Show("Type some destination.", "Woops", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
